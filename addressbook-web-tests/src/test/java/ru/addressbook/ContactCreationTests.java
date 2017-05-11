a9398519a6d9468a80212795ce021bc21d761474
Naum.Ginzburg <Naum.Ginzburg@billing.ru>
2017-05-11 21:30:17 +0300

    Task 4

---
+++ addressbook-web-tests/src/test/java/ru/addressbook/ContactCreationTests.java
@@ -0,0 +1,82 @@
+package ru.addressbook;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class ContactCreationTests {
+    InternetExplorerDriver wd;
+    
+    @Before
+    public void setUp() throws Exception {
+        wd = new InternetExplorerDriver();
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        wd.get("http://localhost/addressbook/");
+        login("admin", "secret");
+    }
+    
+    @Test
+    public void testContactCreation() {
+        gotoEditConactPage();
+        fillContactForm(new ShortContactData("Firstname", "Middlename", "Lastname", "aaa@billing.ru"));
+        submitContactInfo();
+        returnToHomePage();
+    }
+
+    private void returnToHomePage() {
+        wd.findElement(By.linkText("home")).click();
+    }
+
+    private void submitContactInfo() {
+        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    }
+
+    private void gotoEditConactPage() {
+        wd.findElement(By.linkText("add new")).click();
+    }
+
+    private void fillContactForm(ShortContactData shortContactData) {
+        wd.findElement(By.name("firstname")).click();
+        wd.findElement(By.name("firstname")).clear();
+        wd.findElement(By.name("firstname")).sendKeys(shortContactData.getFirstname());
+        wd.findElement(By.name("middlename")).click();
+        wd.findElement(By.name("middlename")).clear();
+        wd.findElement(By.name("middlename")).sendKeys(shortContactData.getMiddlename());
+        wd.findElement(By.name("lastname")).click();
+        wd.findElement(By.name("lastname")).clear();
+        wd.findElement(By.name("lastname")).sendKeys(shortContactData.getLastname());
+        wd.findElement(By.name("email")).click();
+        wd.findElement(By.name("email")).clear();
+        wd.findElement(By.name("email")).sendKeys(shortContactData.getEmail());
+    }
+
+    private void login(String username, String password) {
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys(username);
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys(password);
+        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+
+
+    @After
+    public void tearDown() {
+        wd.quit();
+    }
+    
+    public static boolean isAlertPresent(InternetExplorerDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
---
+++ addressbook-web-tests/src/test/java/ru/addressbook/ShortContactData.java
@@ -0,0 +1,31 @@
+package ru.addressbook;
+
+public class ShortContactData {
+    private final String firstname;
+    private final String middlename;
+    private final String lastname;
+    private final String email;
+
+    public ShortContactData(String firstname, String middlename, String lastname, String email) {
+        this.firstname = firstname;
+        this.middlename = middlename;
+        this.lastname = lastname;
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getMiddlename() {
+        return middlename;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
