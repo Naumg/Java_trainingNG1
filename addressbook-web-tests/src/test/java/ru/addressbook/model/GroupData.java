799bac93e8233a39ebba6f3bfbd17a2d9d172e68
Naum.Ginzburg <Naum.Ginzburg@billing.ru>
2017-05-17 15:13:22 +0300

    Compare Groups' list (no lambda in JAVA v.7)

--- addressbook-web-tests/src/test/java/ru/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/model/GroupData.java
@@ -1,14 +1,30 @@
 package ru.addressbook.model;
 
 public class GroupData {
+    private int id;
     private final String name;
     private final String header;
     private final String footer;
 
     public GroupData(String name, String header, String footer) {
+        this.id = 0;
         this.name = name;
         this.header = header;
         this.footer = footer;
+    }
+    public GroupData(int id, String name, String header, String footer) {
+        this.id = id;
+        this.name = name;
+        this.header = header;
+        this.footer = footer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -27,6 +43,7 @@ public class GroupData {
     public String toString() {
         return "GroupData{" +
                 "name='" + name + '\'' +
+                ", id=" + id +
                 '}';
     }
 
@@ -37,13 +54,15 @@ public class GroupData {
 
         GroupData groupData = (GroupData) o;
 
-        if (!name.equals(groupData.name)) return false;
+        if (id != groupData.id) return false;
+        return name != null ? name.equals(groupData.name) : groupData.name == null;
 
-        return true;
     }
 
     @Override
     public int hashCode() {
-        return name.hashCode();
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
     }
 }
--- addressbook-web-tests/src/test/java/ru/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -19,10 +20,15 @@ public class GroupModificationTests extends TestBase {
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("groupname3ed2", "groupheader3ed2", "groupfooter3ed2"));
+        GroupData group = new GroupData(before.get(before.size() - 1).getId(),  "groupname3ed3", "groupheader3ed3", "groupfooter3ed3");
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
+
+        before.remove(before.size() - 1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 }
